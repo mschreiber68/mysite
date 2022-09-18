a49305763fbe8b538927df686baf98198247a3be
Mike Schreiber <michael.schreiber@slickdeals.net>
2022-09-17 17:52:58 -0700

    cars able to move

--- docs/js/games/mathPractice.js
+++ docs/js/games/mathPractice.js
@@ -3,6 +3,62 @@ class MathRaceTrack extends HTMLElement {
         super();
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(MathRaceTrack.template.content.cloneNode(true));
+
+        this._cpu1 = this.shadowRoot.querySelector('.cpu1');
+        this._cpu2 = this.shadowRoot.querySelector('.cpu2');
+        this._player = this.shadowRoot.querySelector('.player');
+    }
+
+    connectedCallback() {
+        this.init();
+    }
+
+    init() {
+        this._cpu1Pos = 0;
+        this._cpu2Pos = 0;
+        this._playerPos = 0;
+        this._cpu1.style.right = '0';
+        this._cpu2.style.right = '0';
+        this._player.style.right = '0';
+        this._done = false;
+    }
+
+    advanceCpu() {
+        if (this._done) {
+            return;
+        }
+
+        this.cpu1Pos = this.cpu1Pos + Math.floor(Math.random() * 4);
+        if (this._cpu1.offsetLeft <= 0) {
+            // Emit CPU 1 wins
+            this._done = true;
+            return;
+        }
+
+        this.cpu2Pos = this.cpu2Pos + Math.floor(Math.random() * 4);
+        if (this._cpu2.offsetLeft <= 0) {
+            // Emit CPU 2 wins
+            this._done = true;
+            return;
+        }
+    }
+
+    get cpu1Pos() {
+        return this._cpu1Pos;
+    }
+
+    set cpu1Pos(pos) {
+        this._cpu1Pos = pos;
+        this._cpu1.style.right = `${pos}px`;
+    }
+
+    get cpu2Pos() {
+        return this._cpu2Pos;
+    }
+
+    set cpu2Pos(pos) {
+        this._cpu2Pos = pos;
+        this._cpu2.style.right = `${pos}px`;
     }
 }
 
@@ -14,6 +70,7 @@ MathRaceTrack.template.innerHTML = `
         background-color: black;
     }
     .lane {
+        position: relative;
         background-color: black;
         height: 50px;
     }
@@ -21,14 +78,27 @@ MathRaceTrack.template.innerHTML = `
         height: 2px;
         background: repeating-linear-gradient(to right, yellow 0, yellow 10px, transparent 10px, transparent 20px);
     }
+    .car {
+        position: absolute; 
+        height: 50px;
+        right: 0;
+    }
     
 </style>
-<div class="lane"></div>
+<div class="lane">
+    <img src="/images/car-green.svg" class="car cpu1">
+</div>
 <div class="laneDivider"></div>
-<div class="lane"></div>
+<div class="lane">
+    <img src="/images/car-blue.svg" class="car cpu2">
+</div>
 <div class="laneDivider"></div>
-<div class="lane"></div>
+<div class="lane">
+    <img src="/images/car-red.svg" class="car player">
+</div>
 `;
+
+
 
 window.customElements.define('x-mathrace-track', MathRaceTrack);
 
@@ -39,6 +109,14 @@ class MathRace extends HTMLElement {
         super();
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(MathRace.template.content.cloneNode(true));
+
+        /** @type MathRaceTrack */
+        this.track = this.shadowRoot.querySelector('x-mathrace-track');
+        this.animationInterval = null;
+    }
+
+    connectedCallback() {
+        this.animationInterval = setInterval(() => this.track.advanceCpu(), 250);
     }
 }
 
@@ -58,6 +136,10 @@ MathRace.template.innerHTML = `
 `;
 
 window.customElements.define('x-mathrace', MathRace);
+
+/////////////////
+
+
 
 ///////////////////////////////////////////////////////////////
 
