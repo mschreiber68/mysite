6e0b489e025a67d9b42599421ae038a6b73e30ee
Mike Schreiber <michael.schreiber@slickdeals.net>
2022-09-30 18:32:57 -0700

    add anchor-sim component

---
+++ docs/js/components/anchor-sim.js
@@ -0,0 +1,45 @@
+const KEYCODE = {
+    ENTER: 13,
+}
+
+class AnchorSim extends HTMLElement {
+    constructor() {
+        super();
+        this.addEventListener('click', this.#onClick.bind(this));
+        this.addEventListener('keydown', this.#onKeyDown.bind(this));
+    }
+
+    connectedCallback() {
+        this.setAttribute('role', 'link');
+        this.setAttribute('tabindex', '0')
+    }
+
+    #onClick() {
+        this.#doLinkAction();
+    }
+
+    #onKeyDown(event) {
+        switch (event.keyCode) {
+            case KEYCODE.ENTER:
+                this.#doLinkAction();
+                break;
+        }
+    }
+
+    #doLinkAction() {
+        const attrs = this
+            .getAttributeNames()
+            .reduce((attrs, attrName) => {
+                attrs[attrName] = this.getAttribute(attrName);
+                return attrs;
+            }, {});
+
+        const a = document.createElement('a');
+        for (const attrName in attrs) {
+            a.setAttribute(attrName, attrs[attrName]);
+        }
+        a.click();
+    }
+}
+
+window.customElements.define('anchor-sim', AnchorSim);
